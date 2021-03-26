64c0dc8f98c768a9b5b81263b44f2e7852726e46
Davi Rodrigues <developer.davi@gmail.com>
2021-03-26 19:09:18 -0300

    feat: update webhook url

--- pages/api/event.js
+++ pages/api/event.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+const WEBHOOK_URL = 'https://chat.spiry.ro/hooks/c4qtci7b5pdb8gwaf8n6iohgya';
+
 const allowedEvents = [
   'deployment',
   'deployment_status'
@@ -24,7 +26,7 @@ async function handleNewDeployment(payload) {
   const { deployment, repository } = payload;
   const { creator } = deployment;
 
-  await axios.post('https://mm.devdavi.com.br/hooks/h9gn9pjse3f49n1gwgte4md8me', {
+  await axios.post(WEBHOOK_URL, {
     username: 'github-deployments',
     icon_url: 'https://static.vecteezy.com/system/resources/previews/001/951/982/non_2x/cute-little-boy-in-rocket-avatar-character-free-vector.jpg',
     text: `**:rocket: ${repository.name} - New deployment**\nDeployment: ${deployment.url}\nDescription: ${deployment.description || ''}\nEnvironment: ${deployment.environment}\nCreated by: ${creator.login}`
@@ -35,7 +37,7 @@ async function handleNewDeploymentStatus(payload) {
   const { deployment, deployment_status, repository } = payload;
   const { creator } = deployment_status;
 
-  await axios.post('https://mm.devdavi.com.br/hooks/h9gn9pjse3f49n1gwgte4md8me', {
+  await axios.post(WEBHOOK_URL, {
     username: 'github-deployments',
     icon_url: 'https://static.vecteezy.com/system/resources/previews/001/951/982/non_2x/cute-little-boy-in-rocket-avatar-character-free-vector.jpg',
     text: `**${getStateIcon(deployment_status.state)} ${repository.name} - Deployment status updated**\nPreview: ${deployment_status.target_url}\nDeployment: ${deployment.url}\nDescription: ${deployment_status.description || ''}\nEnvironment: ${deployment.environment}\nCreated by: ${creator.login}${errorStates.includes(deployment_status.state) ? '\n<!channel> Something went wrong! Please someone take a look at the deployment results.' : ''}`
