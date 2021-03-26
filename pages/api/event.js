bffb5827624b27e5c9be953be5ede0c89ae8e078
Davi Rodrigues <developer.davi@gmail.com>
2021-03-25 23:10:04 -0300

    feat: add github webhooks event handler

---
+++ pages/api/event.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const allowedEvents = [
+  'deployment',
+  'deployment_status'
+];
+
+async function handleNewDeployment(payload) {
+  const { deployment, repository } = payload;
+  const { creator } = deployment;
+
+  await axios.post('https://mm.devdavi.com.br/hooks/h9gn9pjse3f49n1gwgte4md8me', {
+    username: 'github-deployments',
+    icon_url: 'https://static.vecteezy.com/system/resources/previews/001/951/982/non_2x/cute-little-boy-in-rocket-avatar-character-free-vector.jpg',
+    text: `#### ${repository.name} - New deployment :rocket:\n
+      Deployment: ${deployment.url}\n
+      Description: ${deployment.description}\n
+      Environment: ${deployment.environment}\n
+      Created by ${creator.login}`
+  });
+}
+
+async function handleNewDeploymentStatus(payload) {
+  const { deployment, deployment_status, repository } = payload;
+  const { creator } = deployment_status;
+
+  await axios.post('https://mm.devdavi.com.br/hooks/h9gn9pjse3f49n1gwgte4md8me', {
+    username: 'github-deployments',
+    icon_url: 'https://static.vecteezy.com/system/resources/previews/001/951/982/non_2x/cute-little-boy-in-rocket-avatar-character-free-vector.jpg',
+    text: `#### ${repository.name} - Deployment status updated :rocket:\n
+      Preview: ${deployment_status.target_url}\n
+      Status: ${deployment_status.state}\n
+      Deployment: ${deployment.url}\n
+      Description: ${deployment_status.description}\n
+      Environment: ${deployment.environment}\n
+      Created by ${creator.login}`
+  });
+}
+
+export default async (req, res) => {
+  const { body } = req;
+
+  const event = req.headers['x-github-event'];
+
+  if (!allowedEvents.includes(event)) {
+    return res.status(200).end();
+  }
+
+  switch (event) {
+    case 'deployment':
+      await handleNewDeployment(body);
+      break;
+    case 'deployment_status':
+      await handleNewDeploymentStatus(body);
+      break;
+  }
+
+  res.status(200).end();
+}
